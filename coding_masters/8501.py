e94845aced0a5ca551be95ca031aabdebbd1cb61
minjuko <rhalswn0526@gmail.com>
2024-03-17 22:02:57 +0900

    8501 배수 만들기

--- coding_masters/8501.py
+++ coding_masters/8501.py
@@ -1,18 +1,14 @@
 # 배수 만들기
-
+import sys
+input = sys.stdin.readline
 from itertools import permutations
 
 n = int(input())
-nums = list(map(int, input().split()))
-nums.sort(reverse=True) # 내림차순
+nums = list(map(str, input().split()))
+nums.sort(reverse=True)
 
-# 이어붙여 만들 수 있는 가장 큰 300의 배수 구하기
-for p in permutations(nums, n):
-    num = int(''.join(map(str, p)))
-    if num % 30 == 0:
-        print(num)
-        break
-else:
-    print(-1)
-
-
+for i in permutations(nums, n):
+    if int(''.join(i)) % 300 == 0:
+        print(''.join(i))
+        exit()
+print(-1)
