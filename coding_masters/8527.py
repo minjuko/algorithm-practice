dc28bd28c7d6d990e1e607e9e541cac60be90d04
minjuko <rhalswn0526@gmail.com>
2024-03-25 17:25:12 +0900

    8527 리버스 게임

--- coding_masters/8527.py
+++ coding_masters/8527.py
@@ -1,77 +1,31 @@
-# 리버스라는 보드 게임이 있습니다.
-#
-# 게임 규칙은 이러합니다.
-#
-# 두 명이서 가위바위보를 하고,
-#
-# 이긴 사람은 바둑판에 앞면은 검은색, 뒷면은 흰색인 바둑들을 임의로 깔아놓습니다.
-#
-#
-#
-# 가위바위보에서 진 사람은 바둑돌을 뒤집을 수 있는데,
-#
-# 한번 뒤집을 때 해당 열이나 행의 모든 바둑돌을 뒤집어야 합니다.
-#
-#
-#
-# 예를 들어 3 X 3 형태로 바둑돌이 놓여져 있을 때 원하는 바둑돌 하나만 뒤집을 수는 없고,
-#
-# 특정한 열이나 행에 해당하는 바둑돌 3개를 모두 뒤집어야 합니다.
-#
-# 게임에서 이기기 위해서는 원하는 만큼 바둑돌을 뒤집어서 위에서 보이는 흰색 바둑돌의 개수가 최소가 되어야 합니다.
-#
-#
-#
-# 자연수 N이 주어지고, N X N 형태의 바둑돌들이 주어질 때
-#
-# 바둑돌을 뒤집는 사람이 만들 수 있는 위에서 보이는 흰색 바둑돌의 최소 개수를 출력하는 프로그램을 만드세요.
-#
-#
-# 예제 입력1
-#
-# 3
-# BBW
-# WBB
-# WBW
-#
-# 예제 출력1
-#
-# 2
-#
-# 예제 입력2
-#
-# 2
-# BW
-# WB
-#
-# 예제 출력2
-#
-# 0
-#
-#
-# 입력값 설명
-#
-# 첫째 줄에 자연수 N이 주어집니다. (1 ≤ N ≤ 10)
-#
-# 둘째 줄부터 N개의 줄에 걸쳐 바둑돌들의 상태가 길이가 N인 문자열로 주어집니다.
-#
-# i번째 문자열의 j번째 문자가
-# 'B'인 경우 i행 j열의 바둑돌의 윗면이 검은색이고,
-# 'W'인 경우 윗면이 흰색이라는 의미입니다.
-#
-# 출력값 설명
-#
-# 한 행 또는 한 열에 놓인 N개의 바둑돌을 모두 뒤집는 작업들을 무한정 수행할 수 있다고 했을 때,
-# 윗면이 흰색이 되는 바둑돌의 최소 개수를 출력합니다.
+# 리버스 게임
 
 n = int(input())
-board = [input() for _ in range(n)]
+board = [list(input().rstrip()) for _ in range(n)]
+answer = n*n
 
-def min_white():
+# 뒤집힌 판
+board2 = [k[:] for k in board]
+for i in range(n):
+    for j in range(n):
+        if board2[i][j] == 'B':
+            board2[i][j] = 'W'
+        else: board2[i][j] = 'B'
+
+for w in range(1 << n) :
+    tmp = []
+    for i in range(n) :
+        if w & (1 << i) :
+            tmp.append(board2[i])
+        else : tmp.append(board[i])
+
     cnt = 0
-    for i in range(n):
-        cnt += min(board[i].count('W'), board[i].count('B'))
-    return cnt
+    for i in range(n) :
+        col_count = 0
+        for j in range(n) :
+            if tmp[j][i] == 'W' :
+                col_count += 1
+        cnt += min(col_count, n - col_count)
+    answer = min(cnt, answer)
 
-print(min_white())
-
+print(answer)
