c867ea4c5733953c3eef77d92a5883f3d28369b2
minjuko <rhalswn0526@gmail.com>
2024-05-28 10:13:00 +0900

    8744

--- coding_masters2/고급/8742.py
+++ coding_masters2/고급/8742.py
@@ -1,1 +1,20 @@
 # 심야 버스
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+def floyd():
+    result = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            result[i][j] = graph[i][j]
+        for k in range(n):
+            for w in range(n):
+                for v in range(n):
+                    if result[w][k] and result[k][v]:
+                        result[w][v] = 1
+    return result
+
+result = floyd()
+for i in range(n):
+    print(*result[i])
