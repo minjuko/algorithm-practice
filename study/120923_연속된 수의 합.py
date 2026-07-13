7917aa3e487af7b8a3a6b8b182dada93780b48bb
minjuko <rhalswn0526@gmail.com>
2026-07-13 20:27:27 +0900

    [프로그래머스 120923] 연속된 수의 합

---
+++ study/120923_연속된 수의 합.py
@@ -0,0 +1,7 @@
+# 연속된 수의 합
+def solution(num, total):
+    answer = []
+    start = (total - num * (num - 1) // 2 ) // num
+    for i in range(num):
+        answer.append(start + i)
+    return answer
