84d94635dc411af85ac02c8723bda589f36b869f
minjuko <rhalswn0526@gmail.com>
2026-07-26 01:06:52 +0900

    [programmers 181921] 배열 만들기 2

---
+++ study/level 0/코딩 기초 트레이닝/181921_배열 만들기 2.py
@@ -0,0 +1,10 @@
+def solution(l, r):
+    answer = []
+
+    for i in range(l, r + 1):
+        if all(ch in "05" for ch in str(i)):
+            answer.append(i)
+
+    if not answer:
+        return [-1]
+    return answer
